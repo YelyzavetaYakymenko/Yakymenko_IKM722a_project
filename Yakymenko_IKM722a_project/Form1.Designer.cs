d0a8c75ab30632f71183f88b0c9e3a729fcf9d14
Yelyzaveta Yakymenko IKM722a <yelyzaveta.yakymenko@infiz.khpi.edu.ua>
2024-05-30 11:25:44 +0300

    Finish Task number 1

--- Yakymenko_IKM722a_project/Form1.Designer.cs
+++ Yakymenko_IKM722a_project/Form1.Designer.cs
@@ -40,9 +40,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(394, 182);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.Size = new System.Drawing.Size(31, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Hi";
+            this.label1.Text = "Hello";
             // 
             // tbInput
             // 
