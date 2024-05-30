e1729206a80a2b913898cc177ae1c6c02156c8da
Yelyzaveta Yakymenko IKM722a <yelyzaveta.yakymenko@infiz.khpi.edu.ua>
2024-05-30 13:20:41 +0300

    Finish Task number 4

--- Yakymenko_IKM722a_project/Form1.cs
+++ Yakymenko_IKM722a_project/Form1.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
 
 namespace Yakymenko_IKM722a_project
 {
@@ -49,7 +50,8 @@ namespace Yakymenko_IKM722a_project
                 tClock.Start();
                 bStart.Text = "Стоп"; // зміна тексту на кнопці на "Стоп"
                 this.Mode = false;
-               
+                пускToolStripMenuItem.Text = "Стоп";
+
             }
             else
             {
@@ -60,7 +62,7 @@ namespace Yakymenko_IKM722a_project
                 MajorObject.Write(tbInput.Text);// Запис даних у об'єкт
                 MajorObject.Task();// Обробка даних
                 label1.Text = MajorObject.Read();// Відображення результату
-
+                пускToolStripMenuItem.Text = "Старт";
             }
         }
 
@@ -89,6 +91,59 @@ namespace Yakymenko_IKM722a_project
                 s = (System.DateTime.Now - MajorObject.GetTime()).ToString();
                 MessageBox.Show(s, "Час роботи програми"); // Виведення часу роботи програми і повідомлення "Час роботи програми" на екран
             
+        }
+
+        private void вихідToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void проПрограмуToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            About A = new About();
+            A.ShowDialog();
+        }
+
+        private void зберегтиЯкToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (sfdSave.ShowDialog() == DialogResult.OK)
+
+{
+                MessageBox.Show(sfdSave.FileName);
+            }
+        }
+
+        private void відкритиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (ofdOpen.ShowDialog() == DialogResult.OK) 
+
+{
+                MessageBox.Show(ofdOpen.FileName);
+            }
+        }
+
+        private void проНакопичувачіToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string[] disks = System.IO.Directory.GetLogicalDrives(); // Строковий масив з логічніх дисків
+            string disk = "";
+            for (int i = 0; i < disks.Length; i++)
+            {
+                try
+                {
+                    System.IO.DriveInfo D = new System.IO.DriveInfo(disks[i]);
+                    int BytesInGigabytes = 1073741824;
+                    disk += D.Name + "-" + (D.TotalSize / BytesInGigabytes).ToString() + "-" + (D.TotalFreeSpace / BytesInGigabytes).ToString()
+                   + (char)13;// змінній присвоюється ім’я диска, загальна кількість місця и вільне місце на диску
+
+
+                }
+                catch
+                {
+                    disk += disks[i] + "- не готовий" + (char)13; // якщо пристрій не готовий, то виведення на екран ім’я пристрою і повідомлення «не готовий»
+
+                }
+            }
+            MessageBox.Show(disk, "Накопичувачі");
         }
     }
 
