edd2200d7d5951f325103e0c850521cfdd08532c
Yelyzaveta Yakymenko IKM722a <yelyzaveta.yakymenko@infiz.khpi.edu.ua>
2024-05-30 11:16:53 +0300

    Add project files.

---
+++ Yakymenko_IKM722a_project/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Yakymenko_IKM722a_project
+{
+    public partial class Form1 : Form
+    {
+        private bool Mode;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tClock_Tick(object sender, EventArgs e)
+        {
+            tClock.Stop();
+            MessageBox.Show("Минуло 25 секунд", "Увага");
+            tClock.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Mode = true;
+        }
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            if (Mode)
+            {
+                tbInput.Enabled = true;// Режим дозволу введення
+                tbInput.Focus();
+                tClock.Start();
+                bStart.Text = "Стоп"; // зміна тексту на кнопці на "Стоп"
+                this.Mode = false;
+               
+            }
+            else
+            {
+                tbInput.Enabled = false;// Режим заборони введення
+                tClock.Stop();
+                bStart.Text = "Пуск";// зміна тексту на кнопці на "Пуск"
+                this.Mode = true;
+               
+            }
+        }
+
+      
+
+        private void tbInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            tClock.Stop();
+            tClock.Start();
+            if ((e.KeyChar >= '0') & (e.KeyChar <= '9') | (e.KeyChar == (char)8))
+            {
+                return;
+            }
+            else
+            {
+                tClock.Stop();
+                MessageBox.Show("Неправильний символ", "Помилка");
+                tClock.Start();
+                e.KeyChar = (char)0;
+            }
+        }
+    }
+}
